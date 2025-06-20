481b0913003b5f56bec567246f08896e84d10d4c
arunponugoti1 <160697898+arunponugoti1@users.noreply.github.com>
2025-06-20 11:42:04 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -25,7 +25,7 @@ function App() {
               <div className="space-y-2">
                 <p><span className="font-medium">Date of Birth:</span> October 1st, 1994</p>
                 <p><span className="font-medium">Height:</span> 5' 10"</p>
-                <p><span className="font-medium">Weight:</span> 78 kgs</p>
+                <p><span className="font-medium">Weight:</span> 72 kgs</p>
                 <p><span className="font-medium">Blood Group:</span> O+ve</p>
               </div>
             </section>
