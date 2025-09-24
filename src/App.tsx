d4526e366c115ec11522e355688562d1905a43bb
Arun Ponugoti <160697898+arunponugoti1@users.noreply.github.com>
2025-09-24 09:30:12 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -56,19 +56,6 @@ function App() {
                   <div className="absolute -left-2 w-4 h-4 bg-indigo-600 rounded-full border border-white"></div>
                   <p className="text-base font-medium text-gray-800">Bachelor of Commerce (Computers)</p>
                   <p className="text-sm text-gray-600">Sri Sai Degree & P.G. College, Osmania University</p>
-                  <p className="text-xs text-gray-500">2012 – 2015</p>
-                </li>
-                <li>
-                  <div className="absolute -left-2 w-4 h-4 bg-indigo-600 rounded-full border border-white"></div>
-                  <p className="text-base font-medium text-gray-800">Intermediate (MEC)</p>
-                  <p className="text-sm text-gray-600">Sri Chaitanya Junior College</p>
-                  <p className="text-xs text-gray-500">2010 – 2012</p>
-                </li>
-                <li>
-                  <div className="absolute -left-2 w-4 h-4 bg-indigo-600 rounded-full border border-white"></div>
-                  <p className="text-base font-medium text-gray-800">SSC (10th Class)</p>
-                  <p className="text-sm text-gray-600">Z.P.H.S, Devarakonda</p>
-                  <p className="text-xs text-gray-500">2010</p>
                 </li>
               </ol>
             </section>
